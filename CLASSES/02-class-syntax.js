479a35ebb37a1ad07cebf504d019b0d590e150c2
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-04-28 22:01:55 +0800

    clock code

--- CLASSES/02-class-syntax.js
+++ CLASSES/02-class-syntax.js
@@ -42,3 +42,43 @@
 // const me = new myClass();
 // me.sayHi();
 // console.log(me);
+
+// class User {
+//  constructor(name) {
+//   this.name = name;
+//  }
+
+//  get name() {
+//   return this._name;
+//  }
+//  set name(value) {
+//   if(value.length < 4) {
+//    console.log("Name is too short.");
+//    return
+//   }
+//   this._name = value;
+//  }
+// }
+
+// let user = new User("Mark");
+// console.log(user.name);
+// user.name = "Law";
+// class User {
+//  ['say' + 'Hi']() {
+//   console.log("hello");
+//  }
+// }
+// new User().sayHi();
+
+class Button {
+ constructor(value) {
+  this.value = value;
+ }
+
+ click = () => {
+  console.log(this.value);
+ }
+}
+
+const me = new Button("Hello");
+setTimeout(me.click,1000);
