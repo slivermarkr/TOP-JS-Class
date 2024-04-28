cb7e979ce5967862a91caf397f8f40e6b5a9f178
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-04-28 21:00:23 +0800

    class

---
+++ CLASSES/02-class-syntax.js
@@ -0,0 +1,44 @@
+// class User {
+//  constructor(value){
+//   this.name = value;
+//  }
+
+//  sayHi() {
+//   console.log(`Hi ${this.name}`)
+//  }
+// }
+
+// const mark = new User("Mark");
+// mark.sayHi();
+// let User = class MyClass {
+//  sayHi() {
+//   alert(MyClass);
+//  }
+// }
+
+// new User().sayHi();
+
+// function makeClass(phrase) {
+//  return class {
+//   sayHi() {
+//    console.log(phrase);
+//   }
+//  }
+// }
+
+// const User = makeClass("Hello");
+// new User().sayHi("");
+
+// function createClass(greeting) {
+//  return class {
+//   sayHi() {
+//    console.log(greeting);
+//   }
+//  }
+// }
+
+// const myClass = createClass("Hello");
+
+// const me = new myClass();
+// me.sayHi();
+// console.log(me);
