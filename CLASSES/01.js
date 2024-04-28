2b1fe1f7b4853b734aa846e570e0f8f4cffaa904
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-04-28 18:44:33 +0800

    basic of getter and setter

--- CLASSES/01.js
+++ CLASSES/01.js
@@ -30,18 +30,36 @@
 // console.log(user.fullName);
 // for(let key in user) console.log(key);
 
-let user = {
- get name() {
-  return this._name;
+// let user = {
+//  get name() {
+//   return this._name;
+//  },
+//  set name(value) {
+//   if(value.length < 4) {
+//    console.log("Name is too short...")
+//    return;
+//   }
+//   this._name = value;
+//  }
+// }
+
+// user.name = "Steve";
+// console.log(user.name);
+// user.name = "Law";
+
+const user = {
+ name: "Mark",
+ surname: "Henry",
+
+ get fullName() {
+  return `${this.name} ${this.surname}`;
  },
- set name(value) {
-  if(value.length < 4) {
-   console.log("Name is too short...")
-   return;
-  }
-  this._name = value;
+ set fullName(value) {
+  [this.name, this.surname] = value.split(" ");
  }
-};
-user.name = "Pete"
-console.log(user.name);
-user.name = "";
+
+}
+
+console.log(user.fullName)
+user.fullName = "Jack Black"
+console.log(user.fullName)
