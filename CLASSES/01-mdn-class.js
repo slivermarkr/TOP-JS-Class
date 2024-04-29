4b3b85650c8356a683451e21e7f8f4b4e30df111
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-04-29 22:50:11 +0800

    some more classes

--- CLASSES/01-mdn-class.js
+++ CLASSES/01-mdn-class.js
@@ -21,22 +21,48 @@
 // const tinyang = new Dog("Tiny");
 // tinyang.speak();
 
-class Cat {
- constructor(name) {
-  this.name = name
- }
+// class Cat {
+//  constructor(name) {
+//   this.name = name
+//  }
 
- speak() {
-  console.log(`${this.name} makes a noise.`)
+//  speak() {
+//   console.log(`${this.name} makes a noise.`)
+//  }
+// }
+
+// class Lion extends Cat {
+//  speak() {
+//   super.speak();
+//   console.log(`${this.name} roars.`);
+//  }
+// }
+
+// const l = new Lion("Ming");
+// l.speak();
+
+class MyClass {
+ static staticField = "I am a static field";
+
+ constructor(instanceField) {
+  this.instanceField = instanceField;
+ }
+ static staticMethod() {
+  console.log("This is static method")
+ }
+ instanceMethod() {
+  console.log("This is a instance method")
  }
 }
 
-class Lion extends Cat {
- speak() {
-  super.speak();
-  console.log(`${this.name} roars.`);
- }
-}
+console.log(MyClass.staticField);
+MyClass.staticMethod();
 
-const l = new Lion("Ming");
-l.speak();
+const obj1 = new MyClass("Instance 1")
+const obj2 = new MyClass("Instance 2")
+
+console.log(obj1.staticField)
+console.log(obj1.instanceField);
+
+obj1.staticMethod();
+obj2.instanceMethod();
---
+++ CLASSES/02-using-class.js
@@ -0,0 +1,8 @@
+class Color {
+  constructor(r, g, b) {
+    this.values = [r,g,b];
+  }
+}
+const me = new Color(2,4,5);
+console.log(me.values)
+console.log(me)
