63e6844deb355d49f867270ef10dc285856bca29
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-05-04 21:12:30 +0800

    youtube turorial follow along coding session

---
+++ webpack2.0/webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+ entry: './src/index.js',
+ plugins: [new HtmlWebpackPlugin({
+  template: './src/template.html'
+ })],
+ module: {
+  rules: [
+   {
+    test: /\.scss$/,
+    use: ['style-loader','css-loader','sass-loader']
+   },
+   {
+    test: /\.html$/,
+    use: ['html-loader']
+   },
+   {
+    test: /\.(svg|jpg|png|gif)$/,
+    type: 'asset/resource',
+   },
+  ],
+ } 
+}
