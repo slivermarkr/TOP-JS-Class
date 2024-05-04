fea8e35b94e76dccee59587348454f2f1120125f
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-05-04 11:44:41 +0800

    webpack repo deletion

--- webpack/dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";document.body.appendChild(function(){const e=document.createElement("pre");return e.innerHTML=["Hello webpack!","5 cubed is equal to "+(5,125)].join("\n\n"),e}())})();
