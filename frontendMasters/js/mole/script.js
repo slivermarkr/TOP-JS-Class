7c2ab44018c4a3f793cb3ab57e8a25fd3b832f94
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-09-21 10:06:40 +0800

    sad and hungry mole

--- frontendMasters/js/mole/script.js
+++ frontendMasters/js/mole/script.js
@@ -2,6 +2,24 @@ const HOLE_COUNT = 10;
 
 const wrapper = document.querySelector(".wrapper");
 
+let isHungry = true;
+let nextTime = Date.now();
+
+function next() {
+  const moles = Array.from(document.querySelectorAll(".mole"));
+
+  if (Date.now() > nextTime) {
+    if (isHungry) {
+      moles.forEach((mole) => (mole.src = "./images/king-mole-sad.png"));
+    } else {
+      moles.forEach((mole) => (mole.src = "./images/king-mole-hungry.png"));
+    }
+    isHungry = !isHungry;
+    nextTime = Date.now() + 1000;
+  }
+  requestAnimationFrame(next);
+}
+
 function createMoleHole() {
   const div = document.createElement("div");
   const mole = document.createElement("img");
@@ -20,3 +38,4 @@ function createMultipleHoles() {
 }
 
 createMultipleHoles();
+next();
