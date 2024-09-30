88807081034afaea91cdeb4466f83f79d029aaed
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-09-30 22:22:33 +0800

    js eighteen finished

--- js30/eighteen/main.js
+++ js30/eighteen/main.js
@@ -2,12 +2,17 @@ const nodes = Array.from(document.querySelectorAll("[data-time]"));
 
 function convertToSeconds(time) {
   let result = time.split(":");
-  let seconds = Number(result[0]) * 60 * Number(result[1]);
+  let seconds = Number(result[0]) * 60 + Number(result[1]);
   return seconds;
 }
 
-const totalTime = nodes.reduce((acc, next) => {
+const totalTimeInSeconds = nodes.reduce((acc, next) => {
   return acc + convertToSeconds(next.dataset.time);
 }, 0);
 
-console.log(totalTime / 60);
+let secondsLeft = totalTimeInSeconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+const minutes = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+console.log(`Total watch time: ${hours}:${minutes}:${secondsLeft}`);
